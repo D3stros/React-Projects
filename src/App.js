1c6e9ce5a9f190addda71b3a1c5c884cdeb9d071
Destros <patrick.fallmann@hotmail.com>
2019-09-03 22:23:54 +0200

    Commented out React.createElement

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    // <div className="App">
-    //  <h1>Hi I'm a React App</h1>
-    //  </div>
-    //  );
-    return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Does this work?"));
+     return (
+     <div className="App">
+      <h1>Hi I'm a React App</h1>
+      </div>
+      );
+    // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Does this work?"));
   }
 }
 
