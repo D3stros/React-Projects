fc0ec7447af15770ea75b092f8258f1bff2edeb9
Destros <patrick.fallmann@hotmail.com>
2019-09-04 20:12:58 +0200

    Class based components for managing state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/person";
 
-const app = props => {
-  // Destructuring to pull elements out of an array. React Hooks does not merge with the old state. It completely replaces the old state. In this case is affected.
-  // In setPersonsState the otherState has to be added as well so that it will not be gone after setPersonsState
-  const [ personsState, setPersonsState ]= useState({
+class App extends Component {
+  state = {
     persons: [
       {name: "Max", age: 28},
       {name:"Dennis", age:31},
       {name: "Daniel", age:30}
     ]
-  });
+  }
 
-  const [ otherState, setOtherState] = useState({otherState: "some other value2"});
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // console.log("Was clicked!")
-    setPersonsState({
-      persons: [
+    this.setState({persons: [
       {name: "Maximilian", age: 30},
       {name:"Dennis", age:31},
       {name: "Daniel", age:30}
-    ]
-  });
-  };
+    ]})
+  }
 
+  render() {
      return (
      <div className="App">
       <h1>Hi I'm a React App</h1>
       <p>This is a paragraph.</p>
-      <button onClick= {switchNameHandler}>Switch Name</button>
-      <Person name = {personsState.persons[0].name} age = {personsState.persons[0].age} />
-      <Person name = {personsState.persons[1].name} age = {personsState.persons[1].age} />
-      <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age} />
+      <button onClick= {this.switchNameHandler}>Switch Name</button>
+      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
+      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
      </div>
       );
     // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Does this work?"));
   }
+}
 
-export default app;
-
-
+export default App;
