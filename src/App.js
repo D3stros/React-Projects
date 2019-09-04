869c1fbd226ba734cd981517ee914b0e81e2ebea
Destros <patrick.fallmann@hotmail.com>
2019-09-04 21:00:33 +0200

    Passing method references between components. Check line 29 and line 36. Two different ways to pass method references

--- src/App.js
+++ src/App.js
@@ -11,24 +11,32 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log("Was clicked!")
     this.setState({persons: [
-      {name: "Maximilian", age: 30},
+      {name: newName, age: 30},
       {name:"Dennis", age:31},
       {name: "Daniel", age:30}
     ]})
   }
 
+  // Best practice is bind and not this.switchNameHandler
   render() {
      return (
      <div className="App">
       <h1>Hi I'm a React App</h1>
       <p>This is a paragraph.</p>
-      <button onClick= {this.switchNameHandler}>Switch Name</button>
-      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
-      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
+      <button onClick= {() => this.switchNameHandler("Maximilian")}>Switch Name</button>
+      <Person 
+        name = {this.state.persons[0].name} 
+        age = {this.state.persons[0].age} />
+      <Person 
+        name = {this.state.persons[1].name} 
+        age = {this.state.persons[1].age}
+        click = {this.switchNameHandler.bind(this, "Max!")} />
+      <Person 
+        name = {this.state.persons[2].name} 
+        age = {this.state.persons[2].age} />
      </div>
       );
     // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Does this work?"));
